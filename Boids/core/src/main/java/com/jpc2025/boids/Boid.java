7e1885532d239c03134001226d00cdeb2440d7dc
ErvilhaDev <ervilha.developer@gmail.com>
2025-06-08 20:29:41 -0300

    Boid basic parameter
    parametros basicos do boid adicionados com base no site que o zé mandou

--- Boids/core/src/main/java/com/jpc2025/boids/Boid.java
+++ Boids/core/src/main/java/com/jpc2025/boids/Boid.java
@@ -6,6 +6,34 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Boid {
 	private Sprite sprite;
 	
+	// Parameters
+	float minspeed = 3f;
+	float maxspeed = 6f;
+	float turnfactor = 0.2f;
+	float visualRange = 40f;
+	float protectedRange = 8f;
+	float centeringfactor = 0.0005f;
+	float avoidfactor = 0.05f;
+	float matchingfactor = 0.05f;
+	float maxbias = 0.01f;
+	float bias_increment = 0.00004f;
+	float biasval = 0.001f;
+	
+	// Separation
+	float close_dx;
+	float close_dy;
+	
+	//Alignment
+	float xvel_avg;
+	float yvel_avg;
+	float neighboring_boids;
+	
+	//Cohesion
+	float xpos_avg = 0f;
+	float ypos_avg = 0f;
+	
+	
+	
 	public Boid(Texture texture, float x, float y) {
         this.sprite = new Sprite(texture);
         this.sprite.setPosition(x, y);
